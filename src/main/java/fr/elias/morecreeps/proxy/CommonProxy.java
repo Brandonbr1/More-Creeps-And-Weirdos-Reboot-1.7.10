ac4b6aa7404e21fba2b72810d5b1184ddcd5a6b9
Java sauce <mohammedbrandon204@gmail.com>
2025-08-29 15:59:25 -0400

    ported some particles over to proxy

--- src/main/java/fr/elias/morecreeps/proxy/CommonProxy.java
+++ src/main/java/fr/elias/morecreeps/proxy/CommonProxy.java
@@ -18,83 +18,89 @@ import net.minecraft.world.World;
 
 public class CommonProxy {
 
-  public void render() {}
+    public void render() {}
 
-  public void renderModelItem() {}
+    public void renderModelItem() {}
 
-  public void smoke(World world, EntityPlayer player, Random random) {}
+    public void smoke(World world, EntityPlayer player, Random random) {}
 
-  public void smoke2(World world, Entity entity, Random random) {}
+    public void smoke2(World world, Entity entity, Random random) {}
 
-  public void smoke3(World world, Entity entity, Random random) {}
+    public void smoke3(World world, Entity entity, Random random) {}
 
-  public void blood(World world, double posx, double posy, double posz, boolean realBlood) {}
+    public void blood(World world, double posx, double posy, double posz, boolean realBlood) {}
 
-  public void dirt(World world, EntityPlayer player, Random random, int l, int i1, int k) {}
+    public void dirt(World world, EntityPlayer player, Random random, int l, int i1, int k) {}
 
-  public void foam(World world, EntityPlayer player) {}
+    public void foam(World world, EntityPlayer player) {}
 
-  public void foam2(World world, CREEPSEntityAtom atom) {}
+    public void foam2(World world, CREEPSEntityAtom atom) {}
 
-  public void confettiA(EntityLivingBase player, World world) {
-    if (!world.isRemote) {
-      double d = -MathHelper.sin((player.rotationYaw * (float) Math.PI) / 180F);
-      double d1 = MathHelper.cos((player.rotationYaw * (float) Math.PI) / 180F);
-      CREEPSEntityTrophy creepsentitytrophy = new CREEPSEntityTrophy(world);
-      creepsentitytrophy.setLocationAndAngles(
-          player.posX + d * 3D, player.posY - 2D, player.posZ + d1 * 3D, player.rotationYaw, 0.0F);
-      world.spawnEntityInWorld(creepsentitytrophy);
+    public void confettiA(EntityLivingBase player, World world) {
+        if (!world.isRemote) {
+            double d = -MathHelper.sin((player.rotationYaw * (float) Math.PI) / 180F);
+            double d1 = MathHelper.cos((player.rotationYaw * (float) Math.PI) / 180F);
+            CREEPSEntityTrophy creepsentitytrophy = new CREEPSEntityTrophy(world);
+            creepsentitytrophy.setLocationAndAngles(
+                    player.posX + d * 3D, player.posY - 2D, player.posZ + d1 * 3D, player.rotationYaw, 0.0F);
+            world.spawnEntityInWorld(creepsentitytrophy);
+        }
     }
-  }
 
-  public void shrinkBlast(World world, Entity entity, Random rand) {}
+    public void shrinkBlast(World world, Entity entity, Random rand) {}
 
-  public void shrinkSmoke(World world, Entity entity) {}
+    public void shrinkSmoke(World world, Entity entity) {}
 
-  public void rocketGoBoom(World world, Entity entity, Random rand) {}
+    public void rocketGoBoom(World world, Entity entity, Random rand) {}
 
-  public void rocketSmoke(World world, Entity entity, Item item) {}
+    public void rocketSmoke(World world, Entity entity, Item item) {}
 
-  public void robotTedSmoke(
-      World world, double posX, double posY, double posZ, int floattimer, float modelspeed) {}
+    public void robotTedSmoke(
+            World world, double posX, double posY, double posZ, int floattimer, float modelspeed) {}
 
-  public void confettiB(World world, CREEPSEntityTrophy trophy) {} // for the confetti particles
+    public void confettiB(World world, CREEPSEntityTrophy trophy) {} // for the confetti particles
 
-  public void barf(World world, EntityPlayer player) {}
+    public void barf(World world, EntityPlayer player) {}
 
-  public void bubble(World world, EntityLivingBase entity) {}
+    public void bubble(World world, EntityLivingBase entity) {}
 
-  public void addChatMessage(String s) {}
+    public void addChatMessage(String s) {}
 
-  public void playSoundEffectAtPlayer(World world, String s, float volume, float pitch) {}
+    public void playSoundEffectAtPlayer(World world, String s, float volume, float pitch) {}
 
-  public void pee(
-      World world, double posX, double posY, double posZ, float rotationYaw, float modelsize) {}
+    public void pee(
+            World world, double posX, double posY, double posZ, float rotationYaw, float modelsize) {}
 
-  public void foam3(World world, CREEPSEntityCaveman player, int i, int j, int k) {}
+    public void foam3(World world, CREEPSEntityCaveman player, int i, int j, int k) {}
 
-  public void dirtDigBug(World world, CREEPSEntityDigBug dbug, Random random, int k2) {}
+    public void dirtDigBug(World world, CREEPSEntityDigBug dbug, Random random, int k2) {}
 
-  public void bubbleDoghouse(World world, EntityLivingBase entity) {}
+    public void bubbleDoghouse(World world, EntityLivingBase entity) {}
 
-  public void growParticle(World world, Entity entity) {}
+    public void growParticle(World world, Entity entity) {}
 
-  public void shrinkParticle(World world, Entity entity) {}
+    public void shrinkParticle(World world, Entity entity) {}
 
-  public void smokeHorseHead(World world, CREEPSEntityHorseHead horsehead, Random rand) {}
+    public void smokeHorseHead(World world, CREEPSEntityHorseHead horsehead, Random rand) {}
 
-  public void eatingParticles(Entity entity, World world, int iterAmm) {}
+    public void eatingParticles(Entity entity, World world, int iterAmm) {}
 
-  public void spawnSpit(World world, CREEPSEntityCamel entity, int iterAmm) {}
+    public void spawnSpit(World world, CREEPSEntityCamel entity, int iterAmm) {}
 
-  public void giraffeEatDirt(
-      World world, fr.elias.morecreeps.common.entity.nice.CREEPSEntityRocketGiraffe giraffe) {}
+    public void giraffeEatDirt(
+            World world, fr.elias.morecreeps.common.entity.nice.CREEPSEntityRocketGiraffe giraffe) {}
 
-  public boolean isJumpKeyDown() {
-    return false;
-  }
+    public boolean isJumpKeyDown() {
+        return false;
+    }
 
-  public void smokeRay(World worldObj, CREEPSEntityRay creepsEntityRay, byte byte0) {}
+    public void smokeRay(World worldObj, CREEPSEntityRay creepsEntityRay, byte byte0) {}
 
-  public void spawnBubbles(Entity entity, World world, int iterAmm) {}
+    public void spawnBubbles(Entity entity, World world, int iterAmm) {}
+
+    public void spawnLollimanConfetti(World world, Entity entity, Random rand, int iterAmm1, int iterAmm2){}
+
+    public void kingSmoke(World world, Entity entity, Random random) {}
+
+    public void spawnSalparticles(World world, Entity entity, Random random) {}
 }
