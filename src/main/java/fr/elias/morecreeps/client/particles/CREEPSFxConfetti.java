acee8edb04e1adb14518ffde5ef9a6007f125fda
Java sauce <mohammedbrandon204@gmail.com>
2025-08-29 16:56:57 -0400

    add spotless scrips and apply it to code

--- src/main/java/fr/elias/morecreeps/client/particles/CREEPSFxConfetti.java
+++ src/main/java/fr/elias/morecreeps/client/particles/CREEPSFxConfetti.java
@@ -7,78 +7,75 @@ import net.minecraft.world.World;
 
 public class CREEPSFxConfetti extends EntityFX {
 
-    public CREEPSFxConfetti(World world, double d, double d1, double d2) {
-        super(world, d, d1, d2, 0.0D, 0.0D, 0.0D);
-        // Below was added in 1.8, may be unneeded
-        // this.particleIcon =
-        // Minecraft.getMinecraft().getRenderItem().getItemModelMesher().getParticleIcon(item);
-        this.setSize(0.5F, 0.5F);
-        this.particleRed = 1.0F;
-        this.particleBlue = 1.0F;
-        this.particleGreen = 1.0F;
-        this.particleGravity = 1.0F;
-        this.particleScale = 2.0F;
-        this.particleMaxAge = 55;
+  public CREEPSFxConfetti(World world, double d, double d1, double d2) {
+    super(world, d, d1, d2, 0.0D, 0.0D, 0.0D);
+    // Below was added in 1.8, may be unneeded
+    // this.particleIcon =
+    // Minecraft.getMinecraft().getRenderItem().getItemModelMesher().getParticleIcon(item);
+    this.setSize(0.5F, 0.5F);
+    this.particleRed = 1.0F;
+    this.particleBlue = 1.0F;
+    this.particleGreen = 1.0F;
+    this.particleGravity = 1.0F;
+    this.particleScale = 2.0F;
+    this.particleMaxAge = 55;
+  }
+
+  @Override
+  public int getFXLayer() {
+    return 2;
+  }
+
+  public void func_180434_a(
+      Tessellator p_180434_1_,
+      Entity p_180434_2_,
+      float p_180434_3_,
+      float p_180434_4_,
+      float p_180434_5_,
+      float p_180434_6_,
+      float p_180434_7_,
+      float p_180434_8_) {
+    float f6 = this.particleTextureIndexX / 16.0F;
+    float f7 = f6 + 0.01560938F;
+    float f8 = this.particleTextureIndexY / 16.0F;
+    float f9 = f8 + 0.01560938F;
+    float f10 = 0.1F * this.particleScale;
+
+    if (this.particleIcon != null) {
+      f6 = this.particleIcon.getMinU();
+      f7 = this.particleIcon.getMaxU();
+      f8 = this.particleIcon.getMinV();
+      f9 = this.particleIcon.getMaxV();
     }
 
-    @Override
-    public int getFXLayer() {
-        return 2;
-    }
-
-    public void func_180434_a(
-            Tessellator p_180434_1_,
-            Entity p_180434_2_,
-            float p_180434_3_,
-            float p_180434_4_,
-            float p_180434_5_,
-            float p_180434_6_,
-            float p_180434_7_,
-            float p_180434_8_) {
-        float f6 = this.particleTextureIndexX / 16.0F;
-        float f7 = f6 + 0.01560938F;
-        float f8 = this.particleTextureIndexY / 16.0F;
-        float f9 = f8 + 0.01560938F;
-        float f10 = 0.1F * this.particleScale;
-
-        if (this.particleIcon != null) {
-            f6 = this.particleIcon.getMinU();
-            f7 = this.particleIcon.getMaxU();
-            f8 = this.particleIcon.getMinV();
-            f9 = this.particleIcon.getMaxV();
-        }
-
-        float f11 =
-                (float) (this.prevPosX + (this.posX - this.prevPosX) * p_180434_3_ - interpPosX);
-        float f12 =
-                (float) (this.prevPosY + (this.posY - this.prevPosY) * p_180434_3_ - interpPosY);
-        float f13 =
-                (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * p_180434_3_ - interpPosZ);
-        p_180434_1_.setColorRGBA_F(
-                this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha);
-        p_180434_1_.addVertexWithUV(
-                f11 - p_180434_4_ * f10 - p_180434_7_ * f10,
-                f12 - p_180434_5_ * f10,
-                f13 - p_180434_6_ * f10 - p_180434_8_ * f10,
-                f7,
-                f9);
-        p_180434_1_.addVertexWithUV(
-                f11 - p_180434_4_ * f10 + p_180434_7_ * f10,
-                f12 + p_180434_5_ * f10,
-                f13 - p_180434_6_ * f10 + p_180434_8_ * f10,
-                f7,
-                f8);
-        p_180434_1_.addVertexWithUV(
-                f11 + p_180434_4_ * f10 + p_180434_7_ * f10,
-                f12 + p_180434_5_ * f10,
-                f13 + p_180434_6_ * f10 + p_180434_8_ * f10,
-                f6,
-                f8);
-        p_180434_1_.addVertexWithUV(
-                f11 + p_180434_4_ * f10 - p_180434_7_ * f10,
-                f12 - p_180434_5_ * f10,
-                f13 + p_180434_6_ * f10 - p_180434_8_ * f10,
-                f6,
-                f9);
-    }
+    float f11 = (float) (this.prevPosX + (this.posX - this.prevPosX) * p_180434_3_ - interpPosX);
+    float f12 = (float) (this.prevPosY + (this.posY - this.prevPosY) * p_180434_3_ - interpPosY);
+    float f13 = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * p_180434_3_ - interpPosZ);
+    p_180434_1_.setColorRGBA_F(
+        this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha);
+    p_180434_1_.addVertexWithUV(
+        f11 - p_180434_4_ * f10 - p_180434_7_ * f10,
+        f12 - p_180434_5_ * f10,
+        f13 - p_180434_6_ * f10 - p_180434_8_ * f10,
+        f7,
+        f9);
+    p_180434_1_.addVertexWithUV(
+        f11 - p_180434_4_ * f10 + p_180434_7_ * f10,
+        f12 + p_180434_5_ * f10,
+        f13 - p_180434_6_ * f10 + p_180434_8_ * f10,
+        f7,
+        f8);
+    p_180434_1_.addVertexWithUV(
+        f11 + p_180434_4_ * f10 + p_180434_7_ * f10,
+        f12 + p_180434_5_ * f10,
+        f13 + p_180434_6_ * f10 + p_180434_8_ * f10,
+        f6,
+        f8);
+    p_180434_1_.addVertexWithUV(
+        f11 + p_180434_4_ * f10 - p_180434_7_ * f10,
+        f12 - p_180434_5_ * f10,
+        f13 + p_180434_6_ * f10 - p_180434_8_ * f10,
+        f6,
+        f9);
+  }
 }
